2f4a77d7b414785d534ef3b2012e292c370054aa
hosseinghaedi1999@yahoo.com <Admin5525181>
2020-04-29 22:14:36 +0400

    initial xcode project

---
+++ DevProfile/DevProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevProfile
+//
+//  Created by Hossein Ghaedi on 29/04/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
